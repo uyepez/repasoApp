7d57c72c80309c01971c644234b08fd751b71b04
uyepez <uyepez@tvp.mx>
2022-06-22 18:46:48 -0500

    repaso ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  latitude: Number = 0;
+  longitude: Number = 0;
+  geolocalizacion: any;
+  image: any;
+
+  constructor( private sanitizer: DomSanitizer ) {
+    this.getCurrentPosition();
+  }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('objeto coordenadas', coordinates );
+    this.geolocalizacion = coordinates;
+    this.latitude = this.geolocalizacion.coords.latitude;
+    this.longitude = this.geolocalizacion.coords.longitude;   
+  }
+
+  async openBrowser(){
+    await Browser.open({ url: 'http://capacitorjs.com/' });
+  }
+
+  async takePicture(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64
+    });
+
+    this.image = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/jpeg;base64,${image.base64String}`)
+
+    console.log(image);   
+
+  }
+
 
 }
